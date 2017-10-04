c23d09ac2d5c03bb15799b332832471916021771
Marc Vertido <marc.vertido@gmail.com>
2017-10-03 23:10:07 -0400

    Yay list of high speeds

--- app/src/main/java/com/duraz0rz/suddenpluscalculator/SuddenPlusCalculator.kt
+++ app/src/main/java/com/duraz0rz/suddenpluscalculator/SuddenPlusCalculator.kt
@@ -6,6 +6,18 @@ class SuddenPlusCalculator {
     }
 
     fun generateSuddenPlusTable(bpm: Int, greenNumber: Int): List<SuddenPlusValue> {
-        return listOf()
+        return listOf(
+                SuddenPlusValue(highSpeed = "0.00", greenNumber = 0, whiteNumber = 0, bpm = 0),
+                SuddenPlusValue(highSpeed = "0.50", greenNumber = 0, whiteNumber = 0, bpm = 0),
+                SuddenPlusValue(highSpeed = "1.00", greenNumber = 0, whiteNumber = 0, bpm = 0),
+                SuddenPlusValue(highSpeed = "1.50", greenNumber = 0, whiteNumber = 0, bpm = 0),
+                SuddenPlusValue(highSpeed = "2.00", greenNumber = 0, whiteNumber = 0, bpm = 0),
+                SuddenPlusValue(highSpeed = "2.50", greenNumber = 0, whiteNumber = 0, bpm = 0),
+                SuddenPlusValue(highSpeed = "3.00", greenNumber = 0, whiteNumber = 0, bpm = 0),
+                SuddenPlusValue(highSpeed = "3.50", greenNumber = 0, whiteNumber = 0, bpm = 0),
+                SuddenPlusValue(highSpeed = "4.00", greenNumber = 0, whiteNumber = 0, bpm = 0),
+                SuddenPlusValue(highSpeed = "4.50", greenNumber = 0, whiteNumber = 0, bpm = 0),
+                SuddenPlusValue(highSpeed = "5.00", greenNumber = 0, whiteNumber = 0, bpm = 0)
+        )
     }
 }
--- app/src/main/java/com/duraz0rz/suddenpluscalculator/SuddenPlusValue.kt
+++ app/src/main/java/com/duraz0rz/suddenpluscalculator/SuddenPlusValue.kt
@@ -1,3 +1,3 @@
 package com.duraz0rz.suddenpluscalculator
 
-data class SuddenPlusValue(val highSpeed : Int, val bpm : Int, val whiteNumber: Int, val greenNumber : Int)
+data class SuddenPlusValue(val highSpeed : String, val bpm : Int, val whiteNumber: Int, val greenNumber : Int)
